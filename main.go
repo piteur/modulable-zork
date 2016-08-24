69abe038b569eece898cf9b296ca945e4c51fb88
ppiriou <contact@piteur.fr>
2016-08-24 16:11:29 +0200

    Implement a better story loading logic, with error catching & independents func

--- main.go
+++ main.go
@@ -6,21 +6,72 @@ import (
 	"github.com/piteur/modular-zork/src/loader"
 	"os"
 	"github.com/piteur/modular-zork/src/util"
+	"flag"
+	"github.com/piteur/modular-zork/src/story"
+	"errors"
 )
 
 func main() {
-	fmt.Println("Welcome to 'modular Zork'")
+	var defaultStory, storyToload string
+	defaultStory = "no story selected"
 
-	loader.LoadStories()
-	story := loader.ChooseHistory()
+	// flag configuration
+	flag.StringVar(&storyToload, "story", defaultStory, "path to the story json file")
+	flag.Parse()
 
-	// launch the loaded story
+	// load the story
+	story := getStoryToLoad(storyToload, defaultStory)
+
+	// and launch it
 	exitCode := game.Run(story)
 
 	// wait before killing the program
 	// useful for users who double-click the binary
 	fmt.Println("")
-	_, _ = util.ReadString()
+	util.ReadString()
 
 	os.Exit(exitCode)
+}
+
+// get the story to load based on user input & configuration
+func getStoryToLoad(storyToload string, defaultStory string) (story story.Story) {
+	// no specific story to load: we list the folder & prompt the user
+	if storyToload == defaultStory {
+		fmt.Println("Welcome to 'modular Zork'")
+
+		stories, err := loader.LoadStories("./stories")
+
+		if err != nil {
+			stopOnError(err)
+		}
+
+		return loader.ChooseHistory(stories)
+	}
+
+	// we try to load the specified story
+	story, err := loader.LoadStory(storyToload)
+	err = errors.New("testing stuff")
+
+	if err != nil {
+		stopOnError(err)
+	}
+
+	return
+}
+
+// display a nice formatted message & exit the program
+func stopOnError(err error) {
+	errorMessage := `
+An error occured:
+		%s
+
+If you believe this error shouldn't have happen
+Please feel free to create a new issue on this page: https://github.com/piteur/modular-zork/issues
+
+Sorry for the inconvenience.
+`
+
+	fmt.Printf(errorMessage, err.Error())
+
+	os.Exit(2)
 }
